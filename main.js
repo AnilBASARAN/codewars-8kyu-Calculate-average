c07335b9efb50138410b1faa98a71836c1c4b635
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-01 01:17:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let find_average = (array) => array.length > 0 ? array.reduce((acc,c)=> acc + c, 0)/array.length : 0
+
+
+/*
+
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/javascript
